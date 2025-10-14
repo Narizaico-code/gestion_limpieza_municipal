c242c156b8c57bae40f04782972852a125cb32b6
Narizaico-code <areyes-2024435@kinal.edu.gt>
2025-10-14 02:26:59 -0600

    Commit termine todo

--- src/main/java/org/rsosa/gestion_reportes/dominio/service/ContratacionService.java
+++ src/main/java/org/rsosa/gestion_reportes/dominio/service/ContratacionService.java
@@ -20,7 +20,7 @@ public class ContratacionService {
     }
 
     public ContratacionDto obtenerContratacionPorId(Long id) {
-        return this.contratacionRepository.obtenerContratacionPorId(id);
+        return this.contratacionRepository.obtenerPorCodigo(id);
     }
 
     public ContratacionDto guardarContratacion(ContratacionDto contratacionDto) {
